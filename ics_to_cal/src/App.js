a7f4fe2c1f90cc4448d13907592378a8a54bb6f7
Wesley Ma <wesleykhma@gmail.com>
2018-09-26 02:48:41 -0400

    General look of site

--- ics_to_cal/src/App.js
+++ ics_to_cal/src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import TabView from "./components/TabView";
 import "./styles/App.css";
-import "./styles/bootstrap.min.css";
-import { TabContent, TabPane, Container, Row, Col } from "reactstrap";
-import classnames from "classnames";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <Container>
         <Row>
           <Col>
-            <div>Tabs (Table view or Calendar view)</div>
+            <div>
+              <TabView />
+            </div>
           </Col>
         </Row>
         <Row>
-          <Col>
-            <div>Adding ics files</div>
+          <Col sm="12" md={{ size: 8, offset: 2 }}>
+            Adding ics files
           </Col>
         </Row>
         <Row>
-          <Col>
-            <div>List of ics files</div>
+          <Col sm="12" md={{ size: 8, offset: 2 }}>
+            List of ics files
           </Col>
         </Row>
       </Container>
---
+++ ics_to_cal/src/components/TabView.js
@@ -0,0 +1,86 @@
+import React from "react";
+import classnames from "classnames";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col
+} from "reactstrap";
+
+// Component to swith between viewing schedule events in calendar or table view
+class TabView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "viewCalendar"
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({
+                active: this.state.activeTab === "viewCalendar"
+              })}
+              onClick={() => {
+                this.toggle("viewCalendar");
+              }}
+            >
+              Calendar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({
+                active: this.state.activeTab === "viewTable"
+              })}
+              onClick={() => {
+                this.toggle("viewTable");
+              }}
+            >
+              Events in table
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="viewCalendar">
+            <Row>
+              <Col>
+                <h4>Big ass calendar</h4>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="viewTable">
+            <Row>
+              <Col>
+                <h4>Big ass table</h4>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
+}
+
+export default TabView;
