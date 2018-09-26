91f2a59fb83e2f1606022801e51fc1b23d703ca5
Wesley Ma <wesleykhma@gmail.com>
2018-09-26 01:33:11 -0400

    Installed react, and react strap

---
+++ ics_to_cal/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { TabContent, TabPane, Container, Row, Col } from "reactstrap";
+import classnames from "classnames";
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <div>Tabs (Table view or Calendar view)</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>Adding ics files</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>List of ics files</div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
